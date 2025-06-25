801e73f9a29ec14d2b395fb9eb041acb6df06d0c
100ask <46718957+zhenhuamo@users.noreply.github.com>
2025-06-25 16:55:14 +0800

    master

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -251,6 +251,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
+      url: 'https://rgbatohex.com/tools/color-name',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
